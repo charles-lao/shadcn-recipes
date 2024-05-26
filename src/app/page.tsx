6757b451a73adfbb23d203141b32f6e6da1512e2
charles-lao <charles.h.lao@gmail.com>
2024-05-26 13:39:55 +1200

    add badge button avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
+import { Badge } from "@/components/ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 interface Recipe {
   title: string,
@@ -32,7 +35,12 @@ export default async function Home() {
         {recipes.map(recipe => (
           <Card key={recipe.id} className="flex flex-col justify-between">
             <CardHeader className="flex-row gap-4 items-center">
-              {/* avatar */}
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} alt="recipe img" />
+                <AvatarFallback>
+                  {recipe.title.slice(0,2)}
+                </AvatarFallback>
+              </Avatar>
               <div>
                 <CardTitle>{recipe.title}</CardTitle>
                 <CardDescription>{recipe.time} mins to cook.</CardDescription>
@@ -42,8 +50,8 @@ export default async function Home() {
               <p>{recipe.description}</p>
             </CardContent>
             <CardFooter className="flex justify-between">
-              <button>View Recipe</button>
-              {recipe.vegan && <p>Vegan!</p>}
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
             </CardFooter>
           </Card>
         ))}
